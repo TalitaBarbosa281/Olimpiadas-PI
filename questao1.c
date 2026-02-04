da32d8ffc5a685836e93bfce9584fe241214988a
FelipeCouto01 <luizcoutoof@gmail.com>
2026-02-04 20:58:48 -0300

    Troquei a bios e o results, iniciei a questão 3
    Na questão 3 eu criei um método que me permite pegar qualquer campo de uma linha nos arquivos .csv
    Além disso, fiz o inicio do método para pegar o gênero dos atletas na bios

--- questao1.c
+++ questao1.c
@@ -1,1 +1,1 @@
-//Para um determinado país, determine o número de atletas do sexo masculino e feminino que conquistaram mais de uma medalha de ouro
+//Considere 10 países a sua escolha. Crie um ranking dos países com base no número total de medalhas conquistadas. Ordene os países pelo total de medalhas
--- questao3.c
+++ questao3.c
@@ -1,1 +1,45 @@
-//Considere 10 países a sua escolha. Crie um ranking dos países com base no número total de medalhas conquistadas. Ordene os países pelo total de medalhas
+//Para um determinado país, determine o número de atletas do sexo masculino e feminino que conquistaram mais de uma medalha de ouro
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "structs.h"
+
+#define MAX_LINE 1024
+
+// metodo para pegar o campo de uma linha do csv
+
+char* get_field(char* linha, int num_campo) {
+
+    char* copia_linha = strdup(linha); // Cria uma cópia para não estragar a original
+    char* pedaco = strtok(copia_linha, ","); // Divide a linha em pedaços separados por vírgula
+    char* resultado = NULL; // vai guardar o dado
+    int contador = 0; // vai definir qual a coluna o codigo esta lendo
+
+    while (pedaco != NULL) {
+
+        if (contador == num_campo) {
+            resultado = strdup(pedaco); // Salva o valor para retornar
+            break;
+        }
+        pedaco = strtok(NULL, ","); // faz o codigo continuar lendo a linha de onde parou
+        contador++;
+
+    }
+
+    free(copia_linha); // Limpa a memória
+    return resultado; // Retorna o valor
+}
+
+// metodo para ler o bios.csv buscando o genero dos atletas
+FILE *funcao_bios = fopen("output/bios.csv", "r");
+    if (!funcao_bios) {
+
+        printf("Erro: Nao foi possivel abrir o arquivo bios.csv\n");
+        return;
+
+    }
+
+    fgets(buffer, MAX_LINE, funcao_bios); // Pula cabeçalho, esta função previne o erro de leitura do cabeçalho como dados no while
+
+
